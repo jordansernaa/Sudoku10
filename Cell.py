f8c68754dccd0efddc9d84849be4223202f3c9bc
Jordan Serna <jordanserna256@gmail.com>
2025-12-03 15:55:39 -0500

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -10,8 +10,7 @@ class Cell:
         self.editable = value == 0
         self.selected = False
 
-    def set_cell_value(self, value):
-        self.value = value
+  
 
     def set_sketched_value(self, value):
         self.sketched_value = value
@@ -21,13 +20,3 @@ class Cell:
         small_font = pygame.font.SysFont('Arial', 20)
         x = self.col * 60
         y = self.row * 60
-
-        if self.selected:
-            pygame.draw.rect(self.screen, (255, 0, 0), (x, y, 60, 60), 4)
-
-        if self.value != 0:
-            text = font.render(str(self.value), True, (0, 0, 0))
-            self.screen.blit(text, (x + (60 - text.get_width()) / 2, y + (60 - text.get_height()) / 2))
-        elif self.sketched_value != 0:
-            text = small_font.render(str(self.sketched_value), True, (128, 128, 128))
-            self.screen.blit(text, (x + 5, y + 5))
